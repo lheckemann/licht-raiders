6aba936420dfd6cf750898cc624061fbd200cc6a
Linus <anonymouse2048@gmail.com>
2011-09-07 20:53:31 +0200

    Commented out currently unnecessary function...

--- src/main.cpp
+++ src/main.cpp
@@ -21,14 +21,14 @@ using namespace irr;
 using core::vector3df;
 using video::SColor;
 
-std::vector<std::string> &split(const std::string &s, char delim, std::vector<std::string> &elems) {
+/*std::vector<std::string> &split(const std::string &s, char delim, std::vector<std::string> &elems) {
     std::stringstream ss(s);
     std::string item;
     while(std::getline(ss, item, delim)) {
         elems.push_back(item);
     }
     return elems;
-}
+}*/
 
 IrrlichtDevice *setupDevice(EventReceiver &receiver, ConfigFile *UserConfig) {
 	SIrrlichtCreationParameters params = SIrrlichtCreationParameters();
