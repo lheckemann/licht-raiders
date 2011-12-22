fe6642a5e014c7ec118175c81f5cdd3f9f1cf6a3
Linus <anonymouse2048@gmail.com>
2011-12-22 12:29:44 +0100

    Modified get_irrlicht.sh

--- get_irrlicht.sh
+++ get_irrlicht.sh
@@ -3,10 +3,11 @@
 svn co https://irrlicht.svn.sourceforge.net/svnroot/irrlicht/trunk irrlicht-svn --depth=immediates || (echo "Couldn't checkout irrlicht" ; exit)
 cd irrlicht-svn
 cd source
-svn up
+svn up --depth=files
 cd ../lib
-svn up
+svn up --depth=files
 cd ../include
-svn up
+svn up --depth=files
+cd ..
 patch -p0 -i ../irrlicht-mods.diff
 cd ..
